7a52db0418df9b87e5eaa74a388c15c24dc85f18
Musa Alioğlu <109859611+Alioglumusa@users.noreply.github.com>
2023-03-04 02:12:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-// function imgSlider(anything) {
-//     document.querySelector(".starbucks").src = anything
-// }
+// contact js start
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
